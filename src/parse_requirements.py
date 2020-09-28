fa6208d99ae60f819300a788d6f75d9bd04e86f1
Michelle Xia <michellexia2012@gmail.com>
2020-09-28 14:13:58 -0500

    added margins parsing

--- src/parse_requirements.py
+++ src/parse_requirements.py
@@ -8,7 +8,7 @@ requirements = ['margin', 'margins', 'spacing', 'font', 'size', 'pages', 'spaced
 
 
 def pdf_parser(file_name):
-    """This function takes in a pdf file and returns a list of specifications within the pdf file"""
+    """This function takes in a pdf file and returns a dictionary of the specifications within the pdf file"""
     data = ""
     fp = open(file_name, 'rb')
 
@@ -31,7 +31,9 @@ def pdf_parser(file_name):
     data_list = data.split(" ")
     data_list = clean_list(data_list)
     data_list = parse_specs(data_list)
-    get_specs(data_list)
+    spec_list = get_specs(data_list)
+    print(spec_list)
+    create_dict(spec_list)
 
 
 def clean_list(a_list):
@@ -52,7 +54,38 @@ def get_specs(a_list):
 
     a_list = [item[i] for item in a_list for i in range(len(item))]
 
-    return [val[i] for val in a_list for i in range(len(val))]
+    return [val[i].strip('\"\'') for val in a_list for i in range(len(val))]
+
+
+def create_dict(spec_list):
+    """This function returns a dictionary from the requirements in spec_list"""
+    spec_dict = dict()
+    spec_dict['margins'] = find_margins(spec_list)
+    print(spec_dict)
+
+
+def find_margins(spec_list):
+    # look for 'margins'
+    margin_ind = spec_list.index('margins')
+
+    # look for 'margin'
+    if margin_ind == -1:
+        margin_ind = spec_list.index('margin')
+
+    if margin_ind == -1:
+        return -1
+
+    # look at element before for margin specification
+    if margin_ind > 0:
+        try:
+            return float(spec_list[margin_ind - 1])
+        except ValueError:
+            pass
+    # look at element after for margin specification
+    try:
+        return float(spec_list[margin_ind + 1])
+    except ValueError:
+        return -1
 
 
 def print_words(a_list):
